4f9d6adee16f783f8fc87d65ebf50b371b7a4eef
Mohammad815 <mohammadforhad176@gmail.com>
2022-01-29 01:47:58 +0600

    add

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const { MongoClient } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const cors = require('cors');
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ns6st.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -17,7 +19,16 @@ async function run() {
     try {
       await client.connect();
       console.log('database conntected')
+
+      const database = client.db('travel_agency');
+      const productsCollection = database.collection('users_experience');
      
+      app.post("/AddExperience", async (req, res) => {
+        // console.log(req.body);
+        const result = await productsCollection.insertOne(req.body);
+        res.send(result);
+     });
+
     } finally {
     //   await client.close();
     }
